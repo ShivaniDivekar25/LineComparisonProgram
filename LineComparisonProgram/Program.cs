fa913e3c320a70d401d4efca57580ce944308cda
ShivaniDivekar25 <shivanidivekar2@gmail.com>
2023-02-05 23:05:08 +0530

    Modified to Line Comparison program

--- LineComparisonProgram/Program.cs
+++ LineComparisonProgram/Program.cs
@@ -5,6 +5,30 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Line Comparison Program");
+            //Taking user inputs
+            Console.WriteLine("Please enter option given below");
+            Console.WriteLine("1:Distance between line UC1");
+            int option = Convert.ToInt32(Console.ReadLine());
+            //Uses switch case to call different method
+            switch (option)
+            {
+                case 1:
+                    //Taking user input
+                    Console.WriteLine("Please enter X1 and Y1 coordinates");
+                    var X1 = Convert.ToDouble(Console.ReadLine());
+                    var Y1 = Convert.ToDouble(Console.ReadLine());
+
+                    Console.WriteLine("Please enter X2 and Y2 coordinates");
+                    var X2 = Convert.ToDouble(Console.ReadLine());
+                    var Y2 = Convert.ToDouble(Console.ReadLine());
+                    var finalResult = DistanceBetweenLine.Distance(X1, X2, Y1, Y2);
+
+                    Console.WriteLine("Distance between coordinates {0},{1} and {2},{3} is {4}", X1, Y1, X2, Y2, finalResult);
+                    break;
+                default:
+                    Console.WriteLine("Please enter right option");
+                    break;
+            }
         }
     }
 }
