a1ded836ec31194e34e3536460eed91c2bb8d25e
ShivaniDivekar25 <shivanidivekar2@gmail.com>
2023-02-05 23:25:34 +0530

    Modified to Line Comparison Program

--- LineComparisonProgram/Program.cs
+++ LineComparisonProgram/Program.cs
@@ -7,7 +7,7 @@
             Console.WriteLine("Welcome to Line Comparison Program");
             //Taking user inputs
             Console.WriteLine("Please enter option given below");
-            Console.WriteLine("1:Distance between line UC1");
+            Console.WriteLine("1:Distance between line UC1\n2:Line equality check");
             int option = Convert.ToInt32(Console.ReadLine());
             //Uses switch case to call different method
             switch (option)
@@ -24,6 +24,27 @@
                     var finalResult = DistanceBetweenLine.Distance(X1, X2, Y1, Y2);
 
                     Console.WriteLine("Distance between coordinates {0},{1} and {2},{3} is {4}", X1, Y1, X2, Y2, finalResult);
+                    break;
+                case 2:
+                    {
+                        Console.WriteLine("Please enter X3 and Y3 coordinates");
+                        var X3 = Convert.ToDouble(Console.ReadLine());
+                        var Y3 = Convert.ToDouble(Console.ReadLine());
+
+                        Console.WriteLine("Please enter X4 and Y4 coordinates");
+                        var X4 = Convert.ToDouble(Console.ReadLine());
+                        var Y4 = Convert.ToDouble(Console.ReadLine());
+
+                        Console.WriteLine("Please enter X5 and Y5 coordinates");
+                        var X5 = Convert.ToDouble(Console.ReadLine());
+                        var Y5 = Convert.ToDouble(Console.ReadLine());
+
+                        Console.WriteLine("Please enter X6 and Y6 coordinates");
+                        var X6 = Convert.ToDouble(Console.ReadLine());
+                        var Y6 = Convert.ToDouble(Console.ReadLine());
+
+                        LineEqualityCheck.Equals(X3, Y3, X4, Y4, X5, Y5, X6, Y6);
+                    }
                     break;
                 default:
                     Console.WriteLine("Please enter right option");
